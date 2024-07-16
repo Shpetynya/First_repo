88e55a3363234cc8291d139514b29709c5ccb546
Shpetynya <olga.shpetnaja@gmail.com>
2024-07-16 13:22:04 +0300

    lesson Python Core 2.2

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
-print ('Hello world')
-print('Hello Git')
+# print ('Hello world')
+# print('Hello Git')
+
+
+# Перевіряємо кратність
+# num = int(input())
+
+# if num % 3 == 0 and num % 5 == 0:
+#     print("FizzBuzz")
+# elif num % 3 == 0:
+#     print("Fizz")
+# elif num % 5 == 0:
+#     print("Buzz")
+# else:
+#     print(num)
+# fruit = "apple"
+
+# match fruit:
+#     case "apple":
+#         print("This is an apple.")
+#     case "banana":
+#         print("This is a banana.")
+#     case "orange":
+#         print("This is an orange.")
+#     case _:
+#         print("Unknown fruit.")
+
+'''
+можно использовать начиная с Python 3.10
+
+'''
