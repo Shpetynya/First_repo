44b42a4e02e23dae201dae284f4180cefec08a2a
Shpetynya <olga.shpetnaja@gmail.com>
2024-07-17 21:43:37 +0300

    next lesson

--- main.py
+++ main.py
@@ -44,3 +44,50 @@
 # # Виведення результатів
 # print(f"Загальна кількість символів у рядку: {total_chars}")
 # print(f"Кількість пробілів у рядку: {space_count}")
+
+# задача с использованием ord
+
+# def string_to_codes(string: str) -> dict:
+#     # Ініціалізація словника для зберігання кодів
+#     codes = {}  
+#     # Перебір кожного символу в рядку
+#     for ch in string:  
+#         # Перевірка, чи символ вже є в словнику
+#         if ch not in codes:
+#             # Додавання пари символ-код в словник  
+#             codes[ch] = ord(ch)  
+#     return codes
+
+# result = string_to_codes("Hello world!")
+# print(result)
+
+
+# Factorial
+# def factorial(n):
+#     if n == 0: # базовий випадок
+#         return 1
+#     else:
+#         return n * factorial(n-1) # рекурсивний випадок
+
+# print(factorial(5)) # виведе 120
+
+# Fibonacci
+# def fibonacci(n):
+#     if n <= 1: # базовий випадок
+#         return n
+#     else:
+#         return fibonacci(n-1) + fibonacci(n-2) # рекурсивний випадок
+
+# print(fibonacci(10)) # виведе 55
+
+# def factorial(n):
+#     print("Виклик функції factorial з n = ", n)
+#     if n == 1:
+#         print("Базовий випадок, n = 1, повернення 1")
+#         return 1
+#     else:
+#         result = n * factorial(n-1)
+#         print("Повернення результату для n = ", n, ": ", result)
+#         return result
+
+# print(factorial(5))
